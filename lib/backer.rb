7c2dbffbd1890aeda94f4580437a1027d9a86251
Seth Wax <sethlwax@gmail.com>
2018-04-07 22:21:08 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,16 @@
 class Backer
   attr_reader :name
+  @@backed projects = []
 
   def initialize(name)
     @name = name
+  end
+
+  def self.backed_projects
+    @@backed_projects
+  end
+
+  def self.back_project(project)
+    backed_projects << project
   end
 end
